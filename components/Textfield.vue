9c68f17f4c2fae0bf703f6ba1085fa4856018b4d
Rustum Jordan <rustumjordan@gmail.com>
2023-07-08 15:10:01 +0800

    reconfigure vuetify to the app

---
+++ components/Textfield.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-text-field
+    variant="outlined"
+    v-model="newTask"
+    label="What are you working on?"
+  >
+    <template v-slot:append-inner>
+      <v-fade-transition>
+        <v-btn v-show="newTask" icon="mdi-plus-circle" variant="text"></v-btn>
+      </v-fade-transition>
+    </template>
+  </v-text-field>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    newTask: null,
+  }),
+
+  computed: {},
+
+  methods: {},
+};
+</script>

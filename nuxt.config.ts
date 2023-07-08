0484d6c0be9bd72d40893ebe44623f78e43563d6
Rustum Jordan <rustumjordan@gmail.com>
2023-07-08 13:54:09 +0800

    intallation of vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.scss'],
+  css: ['~/assets/css/main.scss', 'vuetify/styles/main.sass'],
+  build: {
+    transpile: ["vuetify"]
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
